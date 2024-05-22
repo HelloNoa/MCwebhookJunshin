b2137286c051f6c50d49ecd478dcc709691de965
HelloNoa <ohyoungwoo824@gmail.com>
2024-05-22 23:57:12 +0900

    fix func name

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ async def server2():
     os.system('./start2.sh')
 
 @app.get("/")
-async def root():
+async def HealthCheck():
     return {"message": "Hello World"}
 
 @app.get("/MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMXloF6aFigQ6QnDcnox9CsdcCVvCctsRYEtYnN+s0tVrXMzuthCm5y7wGHLrhOD3BpJt6ql4Tx4fkBfJecq378CAwEAAQ==")
-async def root():
+async def restartServer2():
     asyncio.run(server2())
     return {"message": "server1 restart!"}
 
 @app.get("/MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJxauSJISd6+C88OzRfJtX44ZArNGp7R6untVzaLMjL4MHglWSXeFfC/6IaBNXuuaMIah0B69nPDDnRwmD7ED4sCAwEAAQ==")
-def start():
+def restartServer1():
     asyncio.run(server1())
     return {"message": "server2 restart!"}
 
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
