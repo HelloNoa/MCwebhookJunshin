1c2300be5e12160e2b8c612af03615225796c5a4
HelloNoa <ohyoungwoo824@gmail.com>
2024-05-22 23:08:02 +0900

    fix :setting

--- api.sh
+++ api.sh
@@ -1,1 +1,2 @@
 uvicorn main:app --reload --host=0.0.0.0 --port=8001
+#152.67.199.228:25565
--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
-tmux attach -t server
-#!/usr/bin/env sh
-# Forge requires a configured set of both JVM and program arguments.
-# Add custom JVM arguments to the user_jvm_args.txt
-# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
-#  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.30/unix_args.txt nogui "$@"
+cd /home/opc
+sh log.sh
+sh run.sh
